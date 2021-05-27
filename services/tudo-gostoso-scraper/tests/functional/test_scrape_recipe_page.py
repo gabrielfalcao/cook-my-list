09a3be2144432497ef66036df0943cb4cb402e8c
Gabriel Falcão (he/him/his) <gabriel.falcao@nytimes.com>
2021-05-27 16:48:19 +0200

    initial scraper

---
+++ services/tudo-gostoso-scraper/tests/functional/test_scrape_recipe_page.py
@@ -0,0 +1,6 @@
+from tudogostoso_scraper.scrapers import TudoGostosoScraper
+
+
+def test_tudogostoso_recipe_page():
+    # Given a scraper instance
+    scraper = TudoGostosoScraper()
---
+++ services/tudo-gostoso-scraper/tudogostoso_scraper/scrapers.py
@@ -0,0 +1,2 @@
+class TudoGostosoScraper(object):
+    pass
