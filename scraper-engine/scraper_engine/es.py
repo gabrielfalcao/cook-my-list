44380e9964b4e7a65f9ad5416f4354bb8c9c123f
Gabriel Falcão (he/him/his) <gabriel.falcao@nytimes.com>
2021-06-03 00:17:58 +0200

    resolve gracefully the elasticsearch hostname

--- scraper-engine/scraper_engine/es.py
+++ scraper-engine/scraper_engine/es.py
@@ -2,14 +2,26 @@ import os
 import socket
 import multiprocessing
 from elasticsearch import Elasticsearch
+from scraper_engine.logs import get_logger
 
 
-hosts = [
-    socket.gethostbyname(
-        os.getenv("ELASTICSEARCH_HOST") or "localhost",
-    )
-]
+logger = get_logger(__name__)
+
+
+def get_elasticsearch_hostname():
+    return os.getenv("ELASTICSEARCH_HOST") or "localhost"
+
+
+def resolve_hostname(hostname, default="localhost") -> str:
+    try:
+        return socket.gethostbyname(hostname)
+    except Exception:
+        logger.warning(
+            f"could not resolve hostname {hostname}. Defaulting to {default}"
+        )
+        return default
 
 
 def connect_to_elasticsearch() -> Elasticsearch:
+    hosts = [resolve_hostname(get_elasticsearch_hostname())]
     return Elasticsearch(hosts, maxsize=multiprocessing.cpu_count())
